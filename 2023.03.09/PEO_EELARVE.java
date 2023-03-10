92994c9fe8fba78120e099502fa7c679ddcc9420
eduard-leibur <eduard.leibur@gmail.com>
2023-03-10 10:29:18 +0200

    10.03 tunnis

--- 2023.03.09/PEO_EELARVE.java
+++ 2023.03.09/PEO_EELARVE.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PEO_EELARVE {
+    static int eelarve(int külalisi) {
+        return külalisi * 10 + 55;
+    }
     public static void main(String[] args) {
+        Scanner sisse = new Scanner(System.in);
+        System.out.print("Mitu külalist on kutsutud? ");
+        int mitu = sisse.nextInt();
+        sisse.nextLine();
+        System.out.print("Informatsioon külaliste kohta: ");
+        String[] info = sisse.nextLine().split(", ");
+        System.out.println("Info: " + Arrays.toString(info));
+        sisse.close();
+        // ? Anna, + Peeter, + Ülle, ? Eva, ? Juhan, + Maria, + Epp
 
     }
 }
