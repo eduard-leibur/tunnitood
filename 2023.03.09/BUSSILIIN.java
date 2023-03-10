ea0ec076706f049e674804b1ad9c1b8b069bb619
eduard-leibur <eduard.leibur@outlook.com>
2023-03-10 17:53:14 +0200

    10.03 kodus

--- 2023.03.09/BUSSILIIN.java
+++ 2023.03.09/BUSSILIIN.java
@@ -1,17 +1,41 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class BUSSILIIN {
-    static String[] väljub(String peatus, ArrayList<String> sihtkohad) {
+    static ArrayList<String> väljub(String peatus, ArrayList<String> sihtkohad) {
         int väljujaid = 0;
-        for (String loetav_peatus: sihtkohad) {
+        Object[] array = sihtkohad.toArray();
+        for (Object loetav_peatus: array) {
             if (Objects.equals(loetav_peatus, peatus)) {
                 väljujaid++;
                 sihtkohad.remove(peatus);
             }
         }
         System.out.println("Väjub " + väljujaid + " inimest");
+        return sihtkohad;
+    }
+    static ArrayList<String> peale(String peatus, ArrayList<String> marsruut) {
+        Scanner sisse = new Scanner(System.in);
+        System.out.println("Mitu inimest? ");
+        int inimesi = sisse.nextInt();
+        sisse.nextLine();
+        ArrayList<String> sihtkohad = new ArrayList<String>(inimesi);
+        for (int i = 0; i <= inimesi; i++) {
+            System.out.println(i + ". reisija sihtkoht: ");
 
+        }
+        sisse.close();
+
+    }
+    public static void main(String[] args) {
+        ArrayList<String> sihtkohad = new ArrayList<>();
+        sihtkohad.add("Kooli");
+        sihtkohad.add("Nõo");
+        sihtkohad.add("Kooli");
+        sihtkohad.add("Tartu");
+        sihtkohad.add("Meeri");
+
+        System.out.println(väljub("Kooli", sihtkohad));
     }
 }
